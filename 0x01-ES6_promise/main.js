69d18460228a6f7d5c62f81e5c7e57d5be0654a7
CletsyMedia <Cletussam12@yahoo.com>
2024-04-24 03:12:03 +0100

    Dont make a promise...if you know you cant keep it

--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import getResponseFromAPI from "./0-promise.js";
+import getFullResponseFromAPI from './1-promise.js';
 
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
+console.log(getFullResponseFromAPI(true));
+console.log(getFullResponseFromAPI(false));
