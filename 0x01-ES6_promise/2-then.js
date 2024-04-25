d48a81c2370f3632baec245a6f4b39302c595df3
CletsyMedia <Cletussam12@yahoo.com>
2024-04-25 01:32:37 +0100

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,7 +6,7 @@ function handleResponseFromAPI(promise){
       console.log("Got a response from the API");
       return { status: 200, body: "success" };
     })
-   .catch((error) => {
+   .catch(() => {
       console.log("Got a response from the API");
       return new Error();
     });
