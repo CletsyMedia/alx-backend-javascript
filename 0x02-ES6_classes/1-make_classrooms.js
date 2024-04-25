c3fae33cffb4c0e6ebe33c4de262d5309927763f
CletsyMedia <Cletussam12@yahoo.com>
2024-04-25 17:33:32 +0100

    Import the ClassRoom class from 0-classroom.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-    const rooms = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
-    return rooms;
+export default function initializeRooms() {
+  const clsA = new ClassRoom(19);
+  const clsB = new ClassRoom(20);
+  const clsC = new ClassRoom(34);
+  const newCls = [clsA, clsB, clsC];
+  return (newCls);
 }
-
-export default initializeRooms
