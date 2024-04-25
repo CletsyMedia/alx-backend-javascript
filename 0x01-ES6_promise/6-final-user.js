4082924b15ef2c00eef771847fc17417f5d27dbb
CletsyMedia <Cletussam12@yahoo.com>
2024-04-25 01:59:34 +0100

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpUserPromise  = signUpUser(firstName, lastName);
-  const uploadPhotoPromise  = uploadPhoto(fileName);
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise ])
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    return values.map((element) => ({
+      status: element.status,
+      value: element.status === 'fulfilled' ? element.value : `${element.reason}`
+    }));
+  });
 }
