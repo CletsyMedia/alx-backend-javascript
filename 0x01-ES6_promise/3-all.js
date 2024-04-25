5deb831cbdc90f3d5658bd99a79eeaa2ef958d77
CletsyMedia <Cletussam12@yahoo.com>
2024-04-25 01:46:20 +0100

    Handling multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,23 @@
 /* eslint-disable */
-
-
 import { uploadPhoto, createUser } from "./utils.js";
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      const { firstName, lastName } = userResponse;
-      console.log(`${photoResponse.body} ${firstName} ${lastName}`);
+export default function handleProfileSignup() {
+  // Initiating both promises separately
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  // Using Promise.all to wait for both promises to resolve
+  return Promise.all([photoPromise, userPromise])
+    .then((responses) => {
+      // Destructuring the responses array to access individual responses
+      const photoResponse = responses[0];
+      const userResponse = responses[1];
+
+      // Logging the desired format to the console
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
+      // Handling error case
       console.log("Signup system offline");
     });
 }
-
-export default handleProfileSignup;
