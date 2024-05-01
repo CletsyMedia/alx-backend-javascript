7a2e4c0b6804d8bab4d58aedc3344651800168fe
CletsyMedia <Cletussam12@yahoo.com>
2024-05-01 02:50:56 +0100

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default function getListStudentIds(arr) {
-  if (Array.isArray(arr)) {
-    return arr.map((student ) => student .id);
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
   }
   return [];
 }
