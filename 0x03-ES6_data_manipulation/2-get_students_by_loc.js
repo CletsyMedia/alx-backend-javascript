252cf96b0151c7b6021180e7e6d7caacd98bbdc0
CletsyMedia <Cletussam12@yahoo.com>
2024-05-01 02:55:25 +0100

    Returns an array of obj

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,3 @@
-//  function getStudentsByLocation that returns an array of objects
 export default function getStudentsByLocation(list, city) {
   return list.filter((obj) => obj.location === city);
 }
