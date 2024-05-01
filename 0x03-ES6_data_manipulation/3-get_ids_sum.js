f6cdae9d0834abe0d653d77d878f7324ffd46ce0
CletsyMedia <Cletussam12@yahoo.com>
2024-05-01 03:02:27 +0100

    Returns the sum of all the student ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
+// Create a function getStudentIdsSum that returns the sum of all the student ids.
 export default function getStudentIdsSum(list) {
-  return list.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return list.reduce((sum, currValue) => sum + currValue.id, 0);
 }
